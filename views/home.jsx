f2d805265bba22acc6fa61afb1ef226a523774ae
crudd12 <crudd12@gmail.com>
2024-03-04 19:23:14 -0800

    Adding home page view

---
+++ views/home.jsx
@@ -0,0 +1,20 @@
+// npm dependency
+const React = require('react')
+// local file system dependency 
+const Def = require('./default')
+
+function home() {
+    return (
+    <Def>
+        <main>
+            <h1>HOME</h1>
+        </main>
+    </Def>
+    )
+}
+
+module.exports = home
+
+
+
+
