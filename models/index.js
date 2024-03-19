b64f8d4073ba956baa04bae43c4f7bb1f10d2e61
crudd12 <crudd12@gmail.com>
2024-03-19 15:25:07 -0700

    added commentSchema and seeder data

--- models/index.js
+++ models/index.js
@@ -5,4 +5,6 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err));
 
+// models being exported --> do it this way so we can include both models with a single include.
 module.exports.Place = require('./places')
+module.exports.Comment = require('./comment')
